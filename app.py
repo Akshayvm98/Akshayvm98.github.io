3c3f0973d9acdc01edf1c1025724572f41969e60
AKSHAY <mrakshayvm@gmail.com>
2018-08-19 16:19:45 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from bottle import route, run, jinja2_view , request
+
+@route('/')
+@jinja2_view('index.html')
+
+def index():
+	return {'name': 'Akshay'}
+
+
+@route('/', method ="post")
+@jinja2_view('index.html')
+
+def form_handler():
+	para = request.forms.get('para')
+	old = request.forms.get('old')
+	new = request.forms.get('new')
+	new_para = para.replace(old,new)
+
+	return {"para":new_para}
+if os.environ.get('APP_LOCATION')=="heroku":
+	run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)))
+run(host='localhost', port=8080, debug=True)
