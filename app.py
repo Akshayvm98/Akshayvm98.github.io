667749460a1a2d8b695a0f3fb8037d083f30f684
AKSHAY <mrakshayvm@gmail.com>
2018-08-19 16:39:36 +0530

    second commit

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from bottle import route, run, jinja2_view , request
 
 @route('/')
 @jinja2_view('index.html')
-
 def index():
 	return {'name': 'Akshay'}
 
 
 @route('/', method ="post")
 @jinja2_view('index.html')
-
 def form_handler():
 	para = request.forms.get('para')
 	old = request.forms.get('old')
