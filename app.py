49e29fb9d961146d6de51253c4dd328f0f8c060e
AKSHAY <mrakshayvm@gmail.com>
2018-08-19 16:37:44 +0530

    second commit

--- app.py
+++ app.py
@@ -18,6 +18,5 @@ def form_handler():
 	new_para = para.replace(old,new)
 
 	return {"para":new_para}
-if os.environ.get('APP_LOCATION')=="heroku":
+
 	run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)))
-else:	run(host='localhost', port=8080, debug=True)
